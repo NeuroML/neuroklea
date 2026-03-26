19a32a5d3c986115de30763de4badbe73a1d53f7
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-03-26 13:37:21 +0000

    feat(utils): default to `auto` if `cheapest` fails for HF

---
+++ utils_pkg/neuroml_ai_utils/errors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Custom errors
+
+File: utils_pkg/neuroml_ai_utils/errors.py
+
+Copyright 2026 Ankur Sinha
+Author: Ankur Sinha <sanjay DOT ankur AT gmail DOT com>
+"""
+
+
+class LLMInitializationError(Exception):
+    pass

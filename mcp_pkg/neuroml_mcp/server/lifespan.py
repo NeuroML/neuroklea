0e39afe288030e51e56f028446a74f2e1b6a5ae3
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2026-06-03 13:23:04 +0100

    feat(mcp): add disk cache management

--- mcp_pkg/neuroml_mcp/server/lifespan.py
+++ mcp_pkg/neuroml_mcp/server/lifespan.py
@@ -13,6 +13,8 @@ import logging
 import aiohttp
 from fastmcp.server.lifespan import lifespan
 
+from ..utils import cleanup_cache_dir, init_cache_dir
+
 logging.basicConfig(
     format="%(name)s (%(levelname)s) >>> %(message)s\n", level=logging.WARNING
 )
@@ -24,12 +26,18 @@ logger.setLevel(logging.DEBUG)
 @lifespan
 async def app_lifespan(server):
     """Life span for server"""
-    logger.info("Server starting up")
+    logger.info("MCP Server starting up")
 
     # add more sessions here as required
     neuromldb_session = aiohttp.ClientSession()
+    init_cache_dir()
+
     try:
         yield {"neuromldb_session": neuromldb_session}
     finally:
-        logger.info("Shutting down")
+        logger.info("MCP Server shutting down")
+
         await neuromldb_session.close()
+        cleanup_cache_dir()
+
+        logger.info("MCP Server shut down")
